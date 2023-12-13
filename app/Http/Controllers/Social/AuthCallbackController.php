d7f1a4f2a5dd39c0f363270c261a9eb96ec5823b
Alex Garrett-Smith <alex@codecourse.com>
2023-12-13 10:52:40 +0000

    Episode 06

--- app/Http/Controllers/Social/AuthCallbackController.php
+++ app/Http/Controllers/Social/AuthCallbackController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Social;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -10,6 +12,17 @@ class AuthCallbackController extends Controller
 {
     public function __invoke()
     {
-        dd(Socialite::driver('twitter')->user());
+        $user = Socialite::driver('twitter')->user();
+
+        auth()->login(
+            User::firstOrCreate([
+                'x_id' => $user->getId(),
+            ], [
+                'name' => $user->getName(),
+                'email' => $user->getEmail(),
+            ])
+        );
+
+        return redirect(RouteServiceProvider::HOME);
     }
 }
--- database/migrations/2023_12_13_104129_add_x_id_to_users_table.php
+++ database/migrations/2023_12_13_104129_add_x_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('x_id')->nullable();
+            $table->string('x_id')->nullable()->unique();
         });
     }
 
