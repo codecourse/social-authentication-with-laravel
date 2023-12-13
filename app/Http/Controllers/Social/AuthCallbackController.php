a864374150b4640467de79f69962ad95ff45ba0a
Alex Garrett-Smith <alex@codecourse.com>
2023-12-13 10:38:48 +0000

    Episode 04

--- app/Http/Controllers/Social/AuthCallbackController.php
+++ app/Http/Controllers/Social/AuthCallbackController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Social;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthCallbackController extends Controller
 {
     public function __invoke()
     {
-        //
+        dd(Socialite::driver('twitter')->user());
     }
 }
